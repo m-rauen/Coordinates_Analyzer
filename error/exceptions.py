5a729359e2ad60dd58339d1fa5471093bf6341ba
Matheus Rauen <matheus.rauen@grad.ufsc.br>
2022-06-28 13:11:33 -0300

    Adding error dir + organizing structure

--- error/exceptions.py
+++ error/exceptions.py
@@ -1,5 +1,5 @@
 import click
-from cli.main import inputFiles
+#from cli.main import inputFiles
 
 def length_exception():
     msg_exception = ("The length of the coordinates aren't the same.\n"
@@ -15,6 +15,3 @@ def mtxrow_exception():
     msg_exception = ("The generated matrices aren't in the same order\n"
                      "Please, check your xyz files!")
     raise click.ClickException(msg_exception)
-
-
-
