579c81f6fead6f3dffbac3f4acecc589d8e3b780
Matheus Rauen C. L <matheus.rauen@grad.ufsc.br>
2023-02-01 15:39:14 -0300

    Adding basic bootstrap layout

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from dash import Dash, html, dcc
+from dash_bootstrap_components.themes import BOOTSTRAP 
 
-from src.components.layout import create_layout
+from components.layout import create_layout
+
 
 def main():
-    app = Dash()
-    app.title - 'Coordinates Analyzer'
+    app = Dash(
+        external_stylesheets=[
+            BOOTSTRAP
+        ]
+    )
+    app.title = 'Coordinates Analyzer'
     app.layout = create_layout(app)
     app.run()
 
